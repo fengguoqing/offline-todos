adecccdb72b030910957694964f354615b571172
fengguoqing <fengguoqing611@gmail.com>
2018-10-21 22:10:14 +0800

    use custom eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,30 @@
 module.exports = {
   root: true,
-  parser: "babel-eslint",
   parserOptions: {
+    parser: "babel-eslint",
     sourceType: "module"
   },
   env: {
     browser: true,
     node: true
   },
-  extends: "standard",
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended"
+  ],
   globals: {
     __static: true
   },
   plugins: ["html"],
   rules: {
-    // allow paren-less arrow functions
-    "arrow-parens": 0,
-    // allow async-await
-    "generator-star-spacing": 0,
+    'global-require': 0,
+    'import/no-unresolved': 0,
+    'no-param-reassign': 0,
+    'no-shadow': 0,
+    'import/extensions': 0,
+    'import/newline-after-import': 0,
+    'no-multi-assign': 0,
+    "semi": ["error", "always"],
     // allow debugger during development
     "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
   }
